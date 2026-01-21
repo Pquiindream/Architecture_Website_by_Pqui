69c355e59d35f6f661c75a00a7ca3ecd522700b0
Jrusyer de The Universe Astronomy <lightnovelcreepypasta000@gmail.com>
2026-01-21 16:23:27 +0700

    check 7

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Architecture_Website_by_Pqui/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
